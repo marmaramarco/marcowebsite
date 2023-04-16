d26a2ed20489438f8770e58577be660ad639c2ab
marmaramarco <130461323+marmaramarco@users.noreply.github.com>
2023-04-16 11:13:13 +0300

    Add files via upload

---
+++ files/scriptMarco.js
@@ -0,0 +1,88 @@
+var window_height, window_width;
+var ismenuopen = false;
+var is_mobile_phone = ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) ? true : false;
+
+
+$( document ).ready(function() {
+	beReadyPage();
+	setTimeout(function() { beReadyPage();}, 100);
+	setTimeout(function() { beReadyPage();}, 1000);
+});
+
+
+$(document).keydown(function(e){
+	
+	
+});
+
+
+
+
+$(window).scroll(function(event){
+	
+	
+	
+	
+});
+
+
+function beReadyPage () {
+	window_height = parseInt($( window ).height());
+	window_width = parseInt($( window ).width());
+	//$(".fixed_menu_logo_part").css("left", ((parseInt($( ".fixed_menu_right_cont" ).width()) - parseInt($( ".fixed_menu_left_cont" ).width()))) / 2);
+	if(ismenuopen) 
+		$(".menu_closer").css("display", window_width > 1086 ? "none" : "block");
+}
+
+
+function openLeftMenu () {
+	$(".fixed_menu_all_buttons_cont").stop();
+	$(".menu_closer").stop();
+	$('.fixed_menu_all_buttons_cont').animate(
+		{ left: ismenuopen ? -200 : 0 }, 200);
+	if(ismenuopen) {
+		$(".menu_closer").fadeOut(200);
+		$(".menu_opener").addClass('fa-bars');
+		$(".menu_opener").addClass('fa');
+		
+		$(".menu_opener").removeClass('fa-regular');
+		$(".menu_opener").removeClass('fa-solid');
+		$(".menu_opener").removeClass('fa-xmark');
+		
+		$("html body").css("overflow-y", "auto");
+		if(!is_mobile_phone) {
+			$(".main_div").css("width", "100%");
+			$(".fixed_menu_right_cont").css("width", parseInt($( ".fixed_menu_right_cont" ).width()) - 14);
+		}
+		console.log(is_mobile_phone);
+	}
+	else {
+		$(".menu_closer").fadeIn(200);
+		$(".menu_opener").removeClass('fa-bars');
+		$(".menu_opener").removeClass('fa');
+		
+		$(".menu_opener").addClass('fa-regular');
+		$(".menu_opener").addClass('fa-solid');
+		$(".menu_opener").addClass('fa-xmark');
+		
+		$("html body").css("overflow-y", "hidden");
+		if(!is_mobile_phone) {
+			$(".main_div").css("width", "calc(100% - 14px)");
+			$(".fixed_menu_right_cont").css("width", parseInt($( ".fixed_menu_right_cont" ).width()) + 14.5);
+		}
+	}
+	//fa-regular fa-solid fa-xmark
+
+	ismenuopen = !ismenuopen;
+
+	//overflow: hidden;
+}
+
+
+
+$( window ).resize(function() {
+	beReadyPage();
+	setTimeout(function() { beReadyPage();}, 100);
+	return;
+});
+
